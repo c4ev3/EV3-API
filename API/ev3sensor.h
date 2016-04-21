a3f0ac24fada3a14d4a8d5fc8623a3e567bcd950
simonRP <simon.rod92@gmail.com>
2016-04-21 15:44:59 +0200

    Cleanup of sensor API
    sensor defines from sensor.h in sensor.c
    unnecessary erased

--- API/ev3sensor.h
+++ API/ev3sensor.h
@@ -48,109 +48,48 @@ void* readSensorData(int sensorPort);
 
 /**
  * Set sensor mode for a specific port.
- * Example: setSensorMode(INPUT_1, EV3_IR_MODE_REMOTE)
+ * Example: setSensorMode(INPUT_1, COL_REFLECT)
  */
 int setSensorMode(int sensorPort, int name);
 
 /**
  * Set sensor mode for a all ports
- * 
+ * Note: Can be only called once
+ * Example: setAllSensorMode(TOUCH_PRESS, US_DIST_MM, NO_SEN, COL_COLOR)
  */
 int setAllSensorMode(int name_1, int name_2, int name_3, int name_4);
 
-// Sensor ports
-#define INPUT_1 0
-#define INPUT_2 1
-#define INPUT_3 2
-#define INPUT_4 3
-
-// EV3 color sensor modes
-#define COLOR_MODE_REFLECT 0
-#define COLOR_MODE_AMBIENT 1
-#define COLOR_MODE_COLOR 2
-
-// EV3 IR sensor modes
-#define EV3_IR_MODE_PROXIMITY 0
-#define EV3_IR_MODE_SEEK 1
-#define EV3_IR_MODE_REMOTE 2
-#define EV3_IR_MODE_REMOTE_A 3
-#define EV3_IR_MODE_S_ALT 4
-#define EV3_IR_MODE_CAL 5
 
 /***********************************/
-// define of Sensor setup
+// Sensor Names
 #define NO_SEN -1
 //TOUCH
 #define TOUCH_PRESS 1
-#define TOUCH_PRESS_CON 121
-#define TOUCH_PRESS_TYPE 16
-#define TOUCH_PRESS_MODE 0
 
 //Light - Reflect
 #define COL_REFLECT 2
-#define COL_REFLECT_CON 122
-#define COL_REFLECT_TYPE 29
-#define COL_REFLECT_MODE 0
 //Light - Ambient
 #define COL_AMBIENT 3
-#define COL_AMBIENT_CON 122
-#define COL_AMBIENT_TYPE 29
-#define COL_AMBIENT_MODE 1
 //Light - Color
 #define COL_COLOR 4
-#define COL_COLOR_CON 122
-#define COL_COLOR_TYPE 29
-#define COL_COLOR_MODE 2
 
 //Gyro - CM
 #define US_DIST_CM 5
-#define US_DIST_CM_CON 122
-#define US_DIST_CM_TYPE 30
-#define US_DIST_CM_MODE 0
 //Gyro - MM
 #define US_DIST_MM 6
-#define US_DIST_MM_CON 122
-#define US_DIST_MM_TYPE 30
-#define US_DIST_MM_MODE 0
 //Gyro - IN
 #define US_DIST_IN 7
-#define US_DIST_IN_CON 122
-#define US_DIST_IN_TYPE 30
-#define US_DIST_IN_MODE 0
 
 //Gyro - angle
 #define GYRO_ANG 8
-#define GYRO_ANG_CON 122
-#define GYRO_ANG_TYPE 32
-#define GYRO_ANG_MODE 0
 //Gyro - rate
 #define GYRO_RATE 9
-#define GYRO_RATE_CON 122
-#define GYRO_RATE_TYPE 32
-#define GYRO_RATE_MODE 1
 
 //IR - Proximity
-#define IR_PROX 10 
-#define IR_PROX_CON  122
-#define IR_PROX_TYPE 33
-#define IR_PROX_MODE 0
+#define IR_PROX 10
 //IR - Seek
-#define IR_SEEK 10 
-#define IR_SEEK_CON  122
-#define IR_SEEK_TYPE 33
-#define IR_SEEK_MODE 1
+#define IR_SEEK 11
 //IR - Remote Control
-#define IR_REMOTE 10 
-#define IR_REMOTE_CON  122
-#define IR_REMOTE_TYPE 33
-#define IR_REMOTE_MODE 2
-
-
-/*typedef   struct
-{
-  DATA8   TOUCH[3] 		= {121, 16, 0};
-  DATA8   US_DIST_CM[3] = {122, 30, 0};
-}
-SENSOR_SETUP;*/
+#define IR_REMOTE 12
 
 #endif // EV3SENSOR_H
