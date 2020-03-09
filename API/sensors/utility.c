3e9c6dff6ab42eb5d074a115e5a970facfaec9af
Jakub Vaněk <vanek.jakub4@seznam.cz>
2020-03-09 23:13:13 +0100

    fix: store new handler mode in the correct place

--- API/sensors/utility.c
+++ API/sensors/utility.c
@@ -5,7 +5,7 @@
 void setUARTSensorHandlerMode(SensorHandler *handler, int port, int type, int mode) {
     int previousMode = handler->currentSensorMode[port];
     setUARTSensorMode(port, type, mode);
-    EV3Color->currentSensorMode[port] = mode;
+    handler->currentSensorMode[port] = mode;
     if (previousMode != mode) {
         Wait(200);
     }
