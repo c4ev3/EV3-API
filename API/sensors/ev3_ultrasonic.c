7e3f5e922ec43fbc6ad77629be610c3d925d00ae
Jakub Vaněk <vanek.jakub4@seznam.cz>
2020-03-10 17:51:11 +0100

    Extend the ultasonic sensor API

--- API/sensors/ev3_ultrasonic.c
+++ API/sensors/ev3_ultrasonic.c
@@ -1,15 +1,34 @@
 #include "inputs/ev3_input_uart.h"
 #include "ev3_command.h"
-#include "ev3_ultrasonic.h"
+#include "ev3_sensors/ev3_ultrasonic.h"
 #include "utility.h"
+#include <limits.h>
 
-#define EV3_ULTRASONIC_SENSOR_TYPE              30
+//
+// PRIVATE DECLARATIONS
+//
+
+static bool initEV3UltrasonicSensor (int port);
+
+static void exitEV3UltrasonicSensor (int port);
+
+static void setEV3UltrasonicSensorMode(int port, int mode);
+
+static int distanceRead(int port, int mode, int divisor);
+
+//
+// GLOBAL DATA
+//
 
 SensorHandler * EV3Ultrasonic = &(SensorHandler){
         .Init = initEV3UltrasonicSensor,
         .Exit = exitEV3UltrasonicSensor,
         .currentSensorMode = {NONE_MODE, NONE_MODE, NONE_MODE, NONE_MODE}
 };
+
+//
+// IMPLEMENTATION
+//
 
 bool initEV3UltrasonicSensor (int port) {
     setEV3UltrasonicSensorMode(port, EV3_ULTRASONIC_SENSOR_DISTANCE_MM_MODE);
@@ -18,35 +37,75 @@ bool initEV3UltrasonicSensor (int port) {
 
 
 int ReadEV3UltrasonicSensorDistance(int port, EV3UltrasonicDistanceUnit mode) {
-    setEV3UltrasonicSensorMode(port, getEV3UltrasonicSensorModeFromDistanceUnit(mode));
-
-    DATA16 data;
-    if (readFromUART(port, (DATA8 *) &data, 2) == -1) {
-        return -1;
-    }
-
-    int distance = data & 0x0FFF;
-    if (mode == EV3_ULTRASONIC_CM) {
-        distance /= 10;
-    }
-    return distance;
-}
-
-int getEV3UltrasonicSensorModeFromDistanceUnit(EV3UltrasonicDistanceUnit mode){
-    if (mode == EV3_ULTRASONIC_IN) {
-        return EV3_ULTRASONIC_SENSOR_DISTANCE_IN_MODE;
-    } else {
-        return EV3_ULTRASONIC_SENSOR_DISTANCE_MM_MODE;
+    switch (mode) {
+        case EV3_ULTRASONIC_CM:
+            return distanceRead(port, EV3_ULTRASONIC_SENSOR_DISTANCE_MM_MODE, 10);
+        case EV3_ULTRASONIC_MM:
+            return distanceRead(port, EV3_ULTRASONIC_SENSOR_DISTANCE_MM_MODE,  1);
+        case EV3_ULTRASONIC_IN:
+            return distanceRead(port, EV3_ULTRASONIC_SENSOR_DISTANCE_IN_MODE,  1);
+        default:
+            return INT_MIN;
     }
 }
 
-bool ReadEV3UltrasonicSensorListen(int port)  {
+int ReadEV3UltrasonicSensorSingleDistance(int port, EV3UltrasonicDistanceUnit mode) {
+    switch (mode) {
+        case EV3_ULTRASONIC_CM:
+            return distanceRead(port, EV3_ULTRASONIC_SENSOR_SINGLE_MM_MODE, 10);
+        case EV3_ULTRASONIC_MM:
+            return distanceRead(port, EV3_ULTRASONIC_SENSOR_SINGLE_MM_MODE,  1);
+        case EV3_ULTRASONIC_IN:
+            return distanceRead(port, EV3_ULTRASONIC_SENSOR_SINGLE_IN_MODE,  1);
+        default:
+            return INT_MIN;
+    }
+}
+
+int ReadEV3UltrasonicSensorDcDistance(int port, EV3UltrasonicDistanceUnit mode) {
+    switch (mode) {
+        case EV3_ULTRASONIC_CM:
+            return distanceRead(port, EV3_ULTRASONIC_SENSOR_DC_MM_MODE, 10);
+        case EV3_ULTRASONIC_MM:
+            return distanceRead(port, EV3_ULTRASONIC_SENSOR_DC_MM_MODE,  1);
+        case EV3_ULTRASONIC_IN:
+            return distanceRead(port, EV3_ULTRASONIC_SENSOR_DC_IN_MODE,  1);
+        default:
+            return INT_MIN;
+    }
+}
+
+int distanceRead(int port, int mode, int divisor) {
+    setEV3UltrasonicSensorMode(port, mode);
+
+    DATA16 data = 0;
+    int retval = readFromUART(port, (DATA8 *) &data, sizeof(data));
+    if (retval < 0)
+        return INT_MIN;
+
+    return data / divisor;
+}
+
+int ReadEV3UltrasonicSensorListen(int port)  {
     setEV3UltrasonicSensorMode(port, EV3_ULTRASONIC_SENSOR_LISTEN_MODE);
 
-    DATA8 data;
-    readFromUART(port, &data, 1);
+    DATA8 data = 0;
+    int retval = readFromUART(port, &data, sizeof(data));
 
-    return data & 0x0F;
+    if (retval < 0)
+        return INT_MIN;
+
+    return data != 0;
+}
+
+int FireEV3UltrasonicSensor(int port) {
+    uint8_t cmd = EV3_ULTRASONIC_FIRE_COMMAND;
+
+    int retval = writeToUART(port, (DATA8*) &cmd, sizeof(cmd));
+    if (retval != sizeof(cmd))
+        return INT_MIN;
+
+    return 0;
 }
 
 void setEV3UltrasonicSensorMode(int port, int mode) {
--- include/ev3_sensors/ev3_ultrasonic.h
+++ include/ev3_sensors/ev3_ultrasonic.h
@@ -4,31 +4,97 @@
 #include <stdbool.h>
 #include "ev3_sensors.h"
 
+// see http://docs.ev3dev.org/projects/lego-linux-drivers/en/ev3dev-buster/sensor_data.html#lego-ev3-us
+
+#define EV3_ULTRASONIC_SENSOR_TYPE              30
+
 #define EV3_ULTRASONIC_SENSOR_DISTANCE_MM_MODE  0
 #define EV3_ULTRASONIC_SENSOR_DISTANCE_IN_MODE  1
 #define EV3_ULTRASONIC_SENSOR_LISTEN_MODE       2
+#define EV3_ULTRASONIC_SENSOR_SINGLE_MM_MODE    3
+#define EV3_ULTRASONIC_SENSOR_SINGLE_IN_MODE    4
+#define EV3_ULTRASONIC_SENSOR_DC_MM_MODE        5
+#define EV3_ULTRASONIC_SENSOR_DC_IN_MODE        6
+#define EV3_ULTRASONIC_SENSOR_DEFAULT_MODE      EV3_ULTRASONIC_SENSOR_DISTANCE_MM_MODE
 
+#define EV3_ULTRASONIC_FIRE_COMMAND  0x11
+
+/**
+ * @brief EV3 Ultrasonic Sensor (LEGO #45504)
+ */
 extern SensorHandler * EV3Ultrasonic;
 
+/**
+ * @brief Units to measure the result in
+ */
 typedef enum EV3UltrasonicDistanceUnit {
+    /**
+     * @brief Return the distance in centimeters
+     * (discard the decimal place of the *-MM measurement)
+     * range: 0-255
+     */
     EV3_ULTRASONIC_CM,
+
+    /**
+     * @brief Return the distance in millimetes (*-MM)
+     * range: 0-255
+     */
     EV3_ULTRASONIC_MM,
-    EV3_ULTRASONIC_IN
+
+    /**
+     * @brief Return the distance in inches (*-IN)
+     * range: 0-1003
+     */
+    EV3_ULTRASONIC_IN,
 } EV3UltrasonicDistanceUnit;
 
 /**
- * Returns the measured distance by the sensor
- * @param port port to which the sensor is connected
- * @param mode wheter to read the distance in cm, mm or inches
- * @return measured value or -1 in case of error
+ * @brief Returns the distance continually measured by the sensor (US-DIST-*)
+ * @param port Port to which the sensor is connected
+ * @param mode Wheter to read the distance in cm, mm or inches
+ * @return measured value or INT_MIN in case of error
  */
 int ReadEV3UltrasonicSensorDistance(int port, EV3UltrasonicDistanceUnit mode);
 
 /**
- * Returns true if sensor finds another ultrasonic sensor reading in distance mode
- * @param port port to which the sensor is connected
- * @return true if there exists another ultrasonic sensor reading in distance mode
+ * @brief Returns the distance measured by the sensor by a single ping (US-SI-*)
+ *
+ * The distance will be measured only once after the mode is set.
+ * To do the measurement again, either switch the sensor mode, or
+ * call the FireEV3UltrasonicSensor() function.
+ *
+ * @param port Port to which the sensor is connected
+ * @param mode Wheter to read the distance in cm, mm or inches
+ * @return measured value or INT_MIN in case of error
+ * @remark Beware, if you switch the sensor modes too often or
+ *         trigger the ping too often, the sensor may lock up for 2-3 seconds.
  */
-bool ReadEV3UltrasonicSensorListen(int port);
+int ReadEV3UltrasonicSensorSingleDistance(int port, EV3UltrasonicDistanceUnit mode);
+
+/**
+ * @brief Returns the distance measured by the sensor in an unknown alternative way (US-DC-*)
+ * @param port Port to which the sensor is connected
+ * @param mode Wheter to read the distance in cm, mm or inches
+ * @return measured value or INT_MIN in case of error
+ * @remark It is not known how this mode is different from US-DIST-*.
+ *         It is recommended to use US-DIST-* as that is
+ *         the mode used in the stock lms2012 VM.
+ */
+int ReadEV3UltrasonicSensorDcDistance(int port, EV3UltrasonicDistanceUnit mode);
+
+/**
+ * @brief Makes the sensor send out a ping for the single measurement mode.
+ * @param port Port to which the sensor is connected
+ * @return 0 or INT_MIN in case of error
+ */
+int FireEV3UltrasonicSensor(int port);
+
+/**
+ * @brief Tries to find another ultrasonic sensor running in distance mode (US-LISTEN)
+ * @param port Port to which the sensor is connected
+ * @return true if there exists another ultrasonic sensor reading in distance mode,
+ *         false if not and finally INT_MIN in case of error
+ */
+int ReadEV3UltrasonicSensorListen(int port);
 
 #endif //EV3_API_EV3_ULTRASONIC_H
